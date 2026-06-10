683d110c5d6c8af9ae7cae3f58197ebd4a9fb7e2
Sipke Schoorstra <sipkeschoorstra@outlook.com>
2026-06-10 18:51:47 +0200

    split runtime activity output reader contract

--- src/Elsa/Workflows/Runtime/Core/Contracts/IRuntimeActivityOutputRegister.cs
+++ src/Elsa/Workflows/Runtime/Core/Contracts/IRuntimeActivityOutputRegister.cs
@@ -2,12 +2,6 @@ using Elsa.Workflows.Runtime.Core.Models;
 
 namespace Elsa.Workflows.Runtime.Core.Contracts;
 
-public interface IRuntimeActivityOutputReader
-{
-    bool TryGet(ActiveActivityOutputKey key, out ActiveActivityOutput output);
-    IReadOnlyCollection<ActiveActivityOutput> GetActivityOutputs(string workflowExecutionId, string activityExecutionId);
-}
-
 public interface IRuntimeActivityOutputRegister : IRuntimeActivityOutputReader
 {
     void Set(ActiveActivityOutput output);
